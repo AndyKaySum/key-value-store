5ea2bb9fa8c05999ea30c60382559ac28b22d2b1
AndyKaySum <N/A>
2023-11-22 21:15:50 -0500

    Updated experiments + write results to csv

--- experiments/part1.rs
+++ experiments/part1.rs
@@ -1,13 +1,14 @@
-use csc443_project::db::Database;
+use csc443_project::{db::Database, util::types::ENTRY_SIZE};
 mod benchmarker;
 mod experiment;
 
 fn main() {
+    const MEMTABLE_MB_SIZE: usize = 10;
     println!("Part 1: Experiment");
-    println!("Memtable Size: 10 MB\n");
+    println!("Memtable Size: {} MB\n", MEMTABLE_MB_SIZE);
 
     let database_alterations =
-        |db: Database| -> Database { db.resize_memtable(10 * 2_usize.pow(20)) };
+        |db: Database| -> Database { db.resize_memtable(MEMTABLE_MB_SIZE * 2_usize.pow(20) / ENTRY_SIZE) };
 
-    experiment::run(Box::new(database_alterations));
+    experiment::run_and_save(Box::new(database_alterations), "part1_experiments");
 }
