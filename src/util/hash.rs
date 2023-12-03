365ff55c4578e0c44a6552e458b0ea5c47b3f94f
AndyKaySum <N/A>
2023-12-02 21:36:18 -0500

    Hasher helpers + struct

---
+++ src/util/hash.rs
@@ -0,0 +1,36 @@
+use super::types::Key;
+use xxhash_rust::xxh3::xxh3_64_with_seed;
+
+pub fn hash(bytes: &[u8], seed: u64) -> u64 {
+    xxh3_64_with_seed(bytes, seed)
+}
+pub fn hash_to_index<T: AsRef<[u8]>>(item: T, seed: u64, arr_len: usize) -> usize {
+    hash(item.as_ref(), seed) as usize % arr_len
+}
+pub fn hash_key(key: Key, seed: u64) -> u64 {
+    hash(&key.to_le_bytes(), seed)
+}
+pub fn hash_key_to_index(key: Key, seed: u64, arr_len: usize) -> usize {
+    hash_key(key, seed) as usize % arr_len
+}
+
+#[derive(Debug, Default)]
+pub struct FastHasher {
+    state: u64,
+    seed: u64,
+}
+
+impl FastHasher {
+    pub fn new(seed: u64) -> Self {
+        Self { state: 0, seed }
+    }
+}
+
+impl std::hash::Hasher for FastHasher {
+    fn write(&mut self, bytes: &[u8]) {
+        hash(bytes, self.seed);
+    }
+    fn finish(&self) -> u64 {
+        self.state
+    }
+}
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,7 @@
 pub mod algorithm;
 pub mod btree_info;
 pub mod filename;
+pub mod hash;
 pub mod macros;
 pub mod system_info;
 pub mod types;
