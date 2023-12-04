bf8146d230a97e780b990155effb901c2449d10d
AndyKaySum <N/A>
2023-12-03 20:10:14 -0500

    brought back first xxhash dependency

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -7,7 +7,8 @@ pub struct BloomHasher {}
 
 impl BloomHasher {
     pub fn hash(bytes: &[u8], seed: u64) -> u64 {
-        twox_hash::xxh3::hash64_with_seed(bytes, seed)
+        //twox_hash::xxh3::hash64_with_seed(bytes, seed)//NOTE: this function won't have a consistent hash across sessions, so we have to use another lib to do this
+        xxhash_rust::xxh3::xxh3_64_with_seed(bytes, seed)
     }
     pub fn hash_to_index<T: AsRef<[u8]>>(item: T, seed: u64, arr_len: usize) -> usize {
         Self::hash(item.as_ref(), seed) as usize % arr_len
